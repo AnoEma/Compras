65a47d7769d54991907c140ceb87de831b56a620
anolol <anoemangi@gmail.com>
2019-08-31 18:32:28 -0300

    The methode

---
+++ Estoque/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double Total() => Preco * Quantidade;
+        
+        public override string ToString() => Nome + ", " + " Unodade: R$ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture) + "\nValor Total da compra:" 
+            + " R$"+Total().ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
---
+++ Estoque/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados da compra");
+            Console.WriteLine();
+            Console.Write("Nome do produto: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Qual é o preço da unidade: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Qual é a quantidade que voce vai levar: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Dade de compra: " + p);
+
+        }
+    }
+}
